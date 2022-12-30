6068064758b441b22caccbca4c3489c0593edbf4
Corbin Crutchley <git@crutchcorn.dev>
2022-12-29 21:21:24 -0800

    Initial work

---
+++ pages/pokemon/_name.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ name }}</h1>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const name = params.name;
+    return { name };
+  },
+  async fetch() {
+    this.posts = await fetch("https://api.nuxtjs.dev/posts").then((res) =>
+      res.json()
+    );
+  },
+};
+</script>
